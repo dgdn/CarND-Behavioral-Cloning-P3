f489aa7816225ead9374d3db3a54f9fb12f9d6a5
donghang <dgdn88@gmail.com>
2018-03-09 00:12:37 +0800

    apply lenet

--- clone.py
+++ clone.py
@@ -2,7 +2,10 @@ import csv
 import cv2
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers.pooling import MaxPooling2D
+from keras.layers import Flatten, Dense, Lambda, Convolution2D, Dropout
+from keras.layers.core import Activation
+from keras.layers.normalization import BatchNormalization
 
 lines = []
 with open('data/driving_log.csv') as csvfile:
@@ -23,11 +26,26 @@ y_train = np.array(measurements)
 print(X_train.shape, y_train.shape)
 
 model = Sequential()
-model.add(Flatten(input_shape=(160, 320, 3)))
+model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160, 320, 3)))
+model.add(Convolution2D(6, 5, 5, border_mode='valid'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Convolution2D(16, 5, 5, border_mode='valid'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Flatten())
+model.add(Dense(120))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Dense(84))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=4)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=10)
 model.save('model.h5')
      
 
